d3619f78be06ee0d66fb61cbe2fa967830b8bfaf
Erin Luu <luuerin@gmail.com>
2016-11-17 10:54:39 -0500

    Initial Commit

---
+++ CoverFlowLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoverFlowLayout
+//
+//  Created by Erin Luu on 2016-11-17.
+//  Copyright © 2016 Erin Luu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoverFlowLayout/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CoverFlowLayout
+//
+//  Created by Erin Luu on 2016-11-17.
+//  Copyright © 2016 Erin Luu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
